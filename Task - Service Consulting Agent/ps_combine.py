39b441d25b1c8531635216231b5cb0ca1ccc9186
tritantran04 <tritantran04@gmail.com>
2025-09-20 14:45:16 +0700

    Thêm file Medical_services_data.json và update chatbot

--- Task - Service Consulting Agent/ps_combine.py
+++ Task - Service Consulting Agent/ps_combine.py
@@ -8,6 +8,7 @@ def package_to_text(data, services = True):
             total_price = 0
             package_name = package["name"]
             package_desc = package["description"]
+            package_age = package["recommended_age"]
             service_texts = []
 
             for sid in package["services_id"]:
@@ -17,7 +18,7 @@ def package_to_text(data, services = True):
                     total_price += service['price']
             
             # nối thành đoạn văn
-            text = f"{package_name}: {package_desc}. Bao gồm các dịch vụ: {', '.join(service_texts)}. Giá gói khám: {total_price}."
+            text = f"{package_name}: {package_desc}. Độ tuổi {package_age}. Bao gồm các dịch vụ: {', '.join(service_texts)}. Giá gói khám: {total_price}"
             texts.append(text)
             
     else:
