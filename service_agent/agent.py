9fb5749624db74a82188bd4de9640b5ba2d738d4
tritantran04 <tritantran04@gmail.com>
2025-10-09 16:09:10 +0700

    Update agent (error)

--- service_agent/agent.py
+++ service_agent/agent.py
@@ -1,19 +1,21 @@
 from pydantic import BaseModel
-from langchain_core.messages import SystemMessage, HumanMessage, RemoveMessage, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage, RemoveMessage, AIMessage, ToolMessage
 from langgraph.graph import MessagesState, START, END, StateGraph
 from langgraph.checkpoint.memory import MemorySaver
 from pydantic import Field
 from langchain_groq import ChatGroq
-from dotenv import load_dotenv, find_dotenv
-from .utils import get_package_by_id, convert_packages_to_str, PACKAGES
-from .prompts import *
-load_dotenv(find_dotenv())
 
-from langchain_core.messages import HumanMessage, AIMessage
-from langgraph.graph import MessagesState
+from typing import Any, Dict, AsyncIterable, Literal
+
+from .utils import get_package_by_id, convert_packages_to_str, PACKAGES
+from .prompts_p import *
+
 import os
 import re
 import json
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 GROQ_API_KEY = os.getenv("GROQ-API-KEY")
 # GROQ_API_KEY = 'GROQ-API-KEY'
@@ -46,6 +48,24 @@ class ListPackages(BaseModel):
 class HistoryStatus(BaseModel):
     """Trạng thái kiểm tra xem câu hỏi hiện tại có cần thêm nội dung bên ngoài hay chỉ dựa vào thông tin trong lịch sử chat là đủ"""
     decision: int = Field(description="0 nếu đủ thông tin trong lịch sử, 1 nếu cần retrieve thêm")
+
+class ResponseFormat(BaseModel):
+    """    Mô hình chuẩn hóa định dạng phản hồi của hệ thống.
+
+    Thuộc tính:
+    ----------
+    status : Literal["input_required", "completed", "error"]
+        Trạng thái phản hồi của hệ thống. Có 3 giá trị hợp lệ:
+        - "input_required": hệ thống yêu cầu thêm dữ liệu đầu vào.
+        - "completed": quá trình xử lý đã hoàn tất thành công.
+        - "error": xảy ra lỗi trong quá trình xử lý.
+
+    message : str
+        Thông báo chi tiết đi kèm trạng thái, có thể mô tả yêu cầu đầu vào,
+        kết quả xử lý, hoặc nội dung lỗi.
+"""
+    status: Literal["input_required", "completed", "error"] = "input_required"
+    message: str
 
 def summarize_history(state: MessagesState):
     messages = state["messages"]
@@ -163,18 +183,27 @@ def answer_with_retrival(state:MessagesState):
     # print('***'*30)
     # print("Gói đã chọn:\n", selected_packages)
     # print('***'*30)
-    sys_msg = ANSWER_WITH_SERVICE_INSTRUCTION.format(selected_packages=selected_packages)
-    response = llm_qwen3_32b_nr.invoke([SystemMessage(content=sys_msg)] + messages)
-    new_memory = state["messages"] + [response]  # state["messages"] lúc này chỉ là summary
-    return {"messages": new_memory}
 
+    sys_msg = ANSWER_WITH_SERVICE_INSTRUCTION.format(selected_packages=selected_packages)
+    llm_struct = llm_qwen3_32b_wr.with_structured_output(ResponseFormat)
+    response = llm_struct.invoke([SystemMessage(content=sys_msg)] + state["messages"])
+
+    # new_memory = state["messages"] + [response]  # state["messages"] lúc này chỉ là summary
+    return {
+        "messages": state["messages"] + [AIMessage(content=response.message)],
+        "structured_response": response
+    }
 
 def answer_without_retrival(state:MessagesState):
     sys_msg = ANSWER_WITHOUT_SERVICE_INSTRUCTION
+    llm_struct = llm_qwen3_32b_wr.with_structured_output(ResponseFormat)
+    response = llm_struct.invoke([SystemMessage(content=sys_msg)] + state["messages"])
 
-    response = llm_qwen3_32b_nr.invoke([SystemMessage(content=sys_msg)] + state["messages"])
-    new_memory = state["messages"] + [response]  # state["messages"] lúc này chỉ là summary
-    return {"messages": new_memory}
+    # new_memory = state["messages"] + [response]  # state["messages"] lúc này chỉ là summary
+    return {
+        "messages": state["messages"] + [AIMessage(content=response.message)],
+        "structured_response": response
+    }
     
 
 def build_graph() -> StateGraph:
@@ -210,35 +239,85 @@ class ServiceAgent:
     def __init__(self):
         self.graph = build_graph()
     
-    def invoke(self, query, sessionId):
-        config = {"configurable":{"thread_id": sessionId}}
-        input_mes = HumanMessage(content= query)
-        return self.graph.invoke({"messages": input_mes},config)
-    
-    def stream(self, query,sessionId):
+    def invoke(self, query, sessionId) -> str:
         config = {"configurable": {"thread_id": sessionId}}
-        input_mes = HumanMessage(content=query)
-        for chunk in self.graph.stream({"messages": input_mes},config,stream_mode="values"):
-            chunk["messages"][-1].pretty_print()
+        self.graph.invoke({"messages": [("user", query)]}, config)        
+        return self.get_agent_response(config)
+    
+    async def stream(self, query, sessionId) -> AsyncIterable[Dict[str, Any]]:
+        inputs = {"messages": [("user", query)]}
+        config = {"configurable": {"thread_id": sessionId}}
+
+        for item in self.graph.stream(inputs, config, stream_mode="values"):
+            message = item["messages"][-1]
+            if (
+                isinstance(message, AIMessage)
+                and message.tool_calls
+                and len(message.tool_calls) > 0
+            ):
+                yield {
+                    "is_task_complete": False, # true
+                    "require_user_input": False,
+                    "content": "Looking up the exchange rates...",
+                }
+
+            elif isinstance(message, ToolMessage):
+                yield {
+                    "is_task_complete": False,
+                    "require_user_input": False,
+                    "content": "Processing the exchange rates..",
+                }            
+        
+        yield self.get_agent_response(config)
+
+        
+    def get_agent_response(self, config):
+        current_state = self.graph.get_state(config)        
+        structured_response = current_state.values.get('structured_response')
+        if structured_response and isinstance(structured_response, ResponseFormat): 
+            if structured_response.status == "input_required":
+                return {
+                    "is_task_complete": False,
+                    "require_user_input": True,
+                    "content": structured_response.message
+                }
+            elif structured_response.status == "error":
+                return {
+                    "is_task_complete": False,
+                    "require_user_input": True,
+                    "content": structured_response.message
+                }
+            elif structured_response.status == "completed":
+                return {
+                    "is_task_complete": True,
+                    "require_user_input": False,
+                    "content": structured_response.message
+                }
+
+        return {
+            "is_task_complete": False,
+            "require_user_input": True,
+            "content": "We are unable to process your request at the moment. Please try again.",
+        }
         
     
 
-# from pprint import pprint
+from pprint import pprint
 
-# if __name__ == "__main__":
-#     config = {"configurable": {"thread_id": "1"}}
-#     print("Khởi tạo đồ thị...")
-#     graph = build_graph()
-#     print("Đồ thị khởi tạo xong.")
-#     while True:
-#         input_text = input("Nhập câu hỏi của bệnh nhân: ")
-#         if input_text.lower() in ['exit', 'quit', 'q']:
-#             break
+if __name__ == "__main__":
+    config = {"configurable": {"thread_id": "1"}}
+    print("Khởi tạo đồ thị...")
+    graph = build_graph()
+    print("Đồ thị khởi tạo xong.")
+    while True:
+        input_text = input("Nhập câu hỏi của bệnh nhân: ")
+        if input_text.lower() in ['exit', 'quit', 'q']:
+            break
 
-#         input_mes = HumanMessage(content=input_text)
+        input_mes = HumanMessage(content=input_text)
 
-#         for chunk in graph.stream({"messages": input_mes}, config, stream_mode="values"):
+        for chunk in graph.stream({"messages": input_mes}, config, stream_mode="values"):
 
-#             chunk["messages"][-1].pretty_print()
+            chunk["messages"][-1].pretty_print()
             
 
